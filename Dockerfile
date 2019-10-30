f3d30ae0666dde95eabafd0645fb6b7dad329075
Ken Moini <ken@kenmoini.com>
2019-10-30 17:25:45 -0500

    added script to pull in self-signed SSL certs and add to Java keystore

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,12 @@ FROM sonarqube:7.7-community
 
 USER root
 
+RUN apt-get install ca-certificates -y
 ADD sonar.properties /opt/sonarqube/conf/sonar.properties
 ADD run.sh /opt/sonarqube/bin/run.sh
+ADD ss-ca-puller.sh /opt/sonarqube/bin/ss-ca-puller.sh
 CMD /opt/sonarqube/bin/run.sh
+RUN /opt/sonarqube/bin/ss-ca-puller.sh
 RUN cp -a /opt/sonarqube/data /opt/sonarqube/data-init && \
 	cp -a /opt/sonarqube/extensions /opt/sonarqube/extensions-init && \
 	chown sonarqube:sonarqube /opt/sonarqube && chmod -R gu+rwX /opt/sonarqube
---
+++ ss-ca-puller.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This script pulls in SSL Certificates and spits em into the certificate store for Java
+
+set -e
+# set -x	## Uncomment for debugging
+
+for CERT in \
+  www.kenmoini.com:443 \
+  idm.fiercesw.network:636
+do
+  echo "Pulling SSL certificate for ${CERT}..."
+  #FILENAME=$(echo $CERT |cut -f1 -d":")
+  FILENAME=${CERT//":"/".p"}
+  CERTNAME=${FILENAME//"."/"-"}
+  echo "Q" | openssl s_client -connect ${CERT} 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/$FILENAME.pem
+  keytool -import -noprompt -storepass changeit -file /tmp/$FILENAME.pem -alias $CERTNAME -keystore $JAVA_HOME/jre/lib/security/cacerts
+done
